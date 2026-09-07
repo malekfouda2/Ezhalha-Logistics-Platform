f5b84e7735eb0bfa1c5c635d99f33fc1a0919874
Razan Sameh <razan.s@wtm.llc>
2026-09-07 16:22:04 +0300

    remove log

--- mobile/src/lib/hooks/createShipment/express/usePaymentStep.ts
+++ mobile/src/lib/hooks/createShipment/express/usePaymentStep.ts
@@ -167,7 +167,6 @@ export function usePaymentStep() {
             ? error.message
             : t("toast.createShipment.express.payLater.errorMessage"),
       });
-      console.error("Error in handlePayLater:", error);
     } finally {
       setIsPayingLater(false);
     }
